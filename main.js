ba527a520670b8c25d7d4c318dfd202eeae65255
Tejashree <sakharetejashree@gmail.com>
2022-06-17 15:36:25 +0530

    commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function calc() {
     var customersAtStart = document.getElementById("start").value;
+    var customersAtStart = parseInt(customersAtStart,10);
     var customersLost = document.getElementById("lost").value;
+    var customersLost = parseInt(customersLost,10);
     
     var rate = (customersLost/customersAtStart);
     var churnRate = (rate*100);
